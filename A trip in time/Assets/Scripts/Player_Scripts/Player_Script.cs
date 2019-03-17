5335c6cb73fc29dcb4b2588942528e90aeab6cde
Spdoriano101 <43067378+Spdoriano101@users.noreply.github.com>
2019-03-17 00:21:12 +0000

    Added the playable character and also added a couple of teh animations, have used different code to get him to move left and right and also managed to get him to flip as well when pressing the a and d keys.

---
+++ A trip in time/Assets/Scripts/Player_Scripts/Player_Script.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Script : MonoBehaviour {
+
+    //COntainer for the rigid body
+    private Rigidbody2D myRigidbody;
+
+    [SerializeField]
+    private float movementSpeed;
+
+    private bool facingRight;
+
+	// Use this for initialization
+	void Start () {
+
+        //always starts game facing right
+        facingRight = true;
+
+        //Reference to the players rigidbody
+        myRigidbody = GetComponent<Rigidbody2D>();
+
+	}
+	
+	//Keeps the movment of the player constant reguardless of FPS
+	void FixedUpdate () {
+
+        float horizontal = Input.GetAxis("Horizontal");
+        
+        //Calls the handleMovement function to use and also links the "Horizontal" data to the HandleMovement function
+        HandleMovement(horizontal);
+
+        //Detects what way the player should face before running script
+        Flip(horizontal);
+
+	}
+
+    //Sets the players movement
+    private void HandleMovement(float horizontal)
+    {
+        //Controls the speed of the characater movement
+        myRigidbody.velocity = new Vector2(horizontal * movementSpeed, myRigidbody.velocity.y);
+
+    }
+
+    //Function used to flip the character
+    private void Flip(float horizontal)
+    {
+
+        //WHen the horizontal is greater than 0, face right, if not and less than 0, face left
+        if (horizontal > 0  && !facingRight || horizontal < 0 && facingRight)
+        {
+
+            //Will make it false when its true and vice versa, controlling the value
+            facingRight = !facingRight; 
+
+            //Multipling the scale, so if -1 * 1 = -1 then the player will face left as left is 
+            //on the negative and if -1 * -1 = 1 then the player will face right as this is on the positive
+            //"theScale" is the scale of the player
+            Vector3 theScale = transform.localScale;
+
+            theScale.x *= -1;
+
+            transform.localScale = theScale;
+
+        }
+
+    }
+}
