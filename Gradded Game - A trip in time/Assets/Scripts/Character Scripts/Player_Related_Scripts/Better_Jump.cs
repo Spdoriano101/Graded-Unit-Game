a20f295f638899af7f8508d2d64c2b65db6716d6
Spdoriano101 <43067378+Spdoriano101@users.noreply.github.com>
2019-03-17 19:19:40 +0000

    Added new audio to the game and also the items as well. however upon testing the game, i need to change some platforms

---
+++ Gradded Game - A trip in time/Assets/Scripts/Character Scripts/Player_Related_Scripts/Better_Jump.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Better_Jump : MonoBehaviour {
+
+    public float fallMultiplier = 3.5f;
+    public float lowJumpMultiplier = 3f;
+
+ 
+
+    Rigidbody2D rb;
+
+    void Awake() {
+
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+
+    private void Update()
+    {
+       
+
+            if (rb.velocity.y < 0)
+            {
+                rb.velocity += Vector2.up * Physics2D.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
+            }
+            else if (rb.velocity.y > 0 && !Input.GetButton("Jump"))
+            {
+
+                rb.velocity += Vector2.up * Physics2D.gravity.y * (lowJumpMultiplier - 1) * Time.deltaTime;
+            }
+        }
+    
+
+}
