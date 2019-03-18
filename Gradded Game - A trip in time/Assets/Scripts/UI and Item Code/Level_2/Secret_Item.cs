480bfbaf4547c401f33daf5186dfe41bf0ddd271
Spdoriano101 <43067378+Spdoriano101@users.noreply.github.com>
2019-03-18 11:23:38 +0000

    Finalised the seret level and also added a new portal move next level step

---
+++ Gradded Game - A trip in time/Assets/Scripts/UI and Item Code/Level_2/Secret_Item.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Secret_Item : MonoBehaviour {
+
+    //Setting the audio source 
+
+    public Score scoreObject;
+
+    public AudioSource Audio;
+
+    // Use this for initialization
+    void Start()
+    {
+
+        //Asking the program to then retrive the audio source file and apply it to Coin_Pickup
+
+
+
+    }
+
+
+    //Unity calls this function when coin touches any other object in the game
+    // If the player touched the coin, it should vanish and the score should increase
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        //Check if the thing we touched was the player 
+        Player playerScript = collision.collider.GetComponent<Player>();
+        //Player2 playerScript2 = collision.collider.GetComponent<Player2>();
+
+        //If the player collides with an object or asset(s) that 
+        //Have a playerscript attached to it
+        if (playerScript)
+        {
+
+
+
+
+            Score.scoreValue += 150;
+            // Destroy the gameObject that this script is attracted to
+            // (the coin) 
+
+
+
+            Destroy(gameObject);
+        }
+    }
+}
