4fdf10ef6050472c2d5eb59317f3c9e0ba62ad0b
Spdoriano101 <43067378+Spdoriano101@users.noreply.github.com>
2019-03-19 16:24:04 +0000

    Added another script to the game that allows me to be able to have text appear at the bottom of the screen to have th eplayer interact with a characater

---
+++ Gradded Game - A trip in time/Assets/Scripts/Turn_Back.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+[ExecuteInEditMode]
+public class Turn_Back : MonoBehaviour
+{
+
+    public bool textDisplay;
+  
+    //public string Text = "Turn Back";
+
+    //Allows for the box that the text is in to be changed
+    public Rect BoxSize = new Rect(0, 0, 200, 100);
+
+   
+    // if this script is on an object with a collider display the Gui
+    void OnTriggerEnter()
+    {
+        textDisplay = true;
+    }
+
+    // if this script is on an object with a collider do not #display the Gui
+    void OnTriggerExit()
+    {
+        textDisplay = false;
+    }
+
+   
+
+}
---
+++ Gradded Game - A trip in time/Assets/Scripts/UI and Item Code/Dialog.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Dialog : MonoBehaviour {
+
+    //Sets varaibles for the script
+    public TextMeshProUGUI textDisplay;
+
+    public string[] sentences;
+
+    private int index;
+
+    public float typingSpeed;
+
+    public GameObject continueButton;
+
+    void Start(){
+
+        StartCoroutine(Type());
+
+    }
+
+    void Update()
+    {
+        //check to see if the current display text is = to the currrent sentence
+        if (textDisplay.text == sentences[index])
+        {
+
+            //displays the continue button once that sentence has been typed out
+            continueButton.SetActive(true);
+
+        }
+
+    }
+
+    IEnumerator Type()
+    {
+
+        foreach (char letter in sentences[index].ToCharArray())
+        {
+
+            textDisplay.text += letter;
+            yield return new WaitForSeconds(typingSpeed);
+
+        }
+
+    }
+
+    public void NextSentence(){
+
+
+        //immediatley hides the continue button after click until the dialog has ended
+        continueButton.SetActive(false);
+
+        if(index < sentences.Length - 1){
+
+            index++;
+
+            textDisplay.text = "";
+
+            StartCoroutine(Type());
+
+        }
+
+        else
+        {
+
+            textDisplay.text = "";
+
+            //Hides the continue button once the final text line has finished
+            continueButton.SetActive(false);
+
+        }
+    }
+}
