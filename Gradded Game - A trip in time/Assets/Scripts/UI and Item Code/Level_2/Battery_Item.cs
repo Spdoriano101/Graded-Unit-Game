a41d4e96e9c360e0403eca6abb21b6304d56ba50
Spdoriano101 <43067378+Spdoriano101@users.noreply.github.com>
2019-05-22 13:24:23 +0100

    Added my timemachine and also my battery, I also added some barrels for a bit of exttra fun and interaction within the game

---
+++ Gradded Game - A trip in time/Assets/Scripts/UI and Item Code/Level_2/Battery_Item.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Battery_Item : MonoBehaviour {
+
+
+    public Score scoreObject;
+
+    public AudioSource Audio;
+
+    // Use this for initialization
+    void Start()
+    {
+
+
+    }
+
+
+    //collision detection
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+       
+        Player playerScript = collision.collider.GetComponent<Player>();
+     
+
+        
+        if (playerScript)
+        {
+
+
+
+            // audio to play when the assets with the player scirpt is collided with
+            Score.scoreValue += 200;
+            // Destroy the gameObject that this script is attacted to
+           
+
+
+            Destroy(gameObject);
+        }
+    }
+}
